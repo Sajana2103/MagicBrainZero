b0676d88dd8ed610954352a4aa89366ad68b21ad
Sajana Bupathi Herath <sajanah555@gmail.com>
2020-12-21 10:35:23 +0530

    modified bcrypt
    uninstalled bcrypt and installed bcrypt-nodejs

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const cors = require('cors')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 
 const knex = require('knex')
 const pg = require('pg')
