786981ba941aa3725dd6e06747cc4c2a6ddf72f3
Sajana Bupathi Herath <sajanah555@gmail.com>
2020-12-21 15:52:35 +0530

    updated bcrypt

--- controllers/register.js
+++ controllers/register.js
@@ -4,8 +4,9 @@ const registerHandling = (req, res, database, bcrypt) => {
         return res.status(400).json("Incorrect for submission")
     }
 
-        const saltRounds = 10;
+        const saltRounds = bcrypt.genSaltSync(10)
         const hashed = bcrypt.hashSync(password, saltRounds)
+
 
         database.transaction(trx => {
             trx.insert({
@@ -21,8 +22,10 @@ const registerHandling = (req, res, database, bcrypt) => {
                             email: loginEmail[0],
                             joined: new Date()
                         })
+
                         .then(user => {
                             res.json(user[0])
+
                         })
                 })
                 .then(trx.commit)
